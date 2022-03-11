25d263900b51a29e64cb6f6760bf5134fab54f62
nzhoward <nzhoward@gmail.com>
2022-03-11 14:55:56 -0500

    avatars

--- ipfs-pychat.py
+++ ipfs-pychat.py
@@ -4,9 +4,15 @@ import base64
 
 listening = True
 sending = True
-id_set = set()
+id_set = {}
+avatars = [u'\U0001F437', u'\U0001F412', u'\U0001F414', u'\U0001F42D', u'\U0001F42F', u'\U0001F431', u'\U0001F43B']
+avatar_idx = 0
 
 def listen(client, channel):
+    global avatar_idx
+    global sending
+
+    # subscribe to dummy channel to get own ipfs ID
     id_self = None
     sub = client.pubsub.subscribe('ping_channel')
     client.pubsub.publish('ping_channel', 'ping')
@@ -15,27 +21,28 @@ def listen(client, channel):
         break
     sub.close()
 
-    print('My ID:', id_self)
-    id_set.add(id_self)
+    id_set[id_self] = avatars[avatar_idx]
+    avatar_idx += 1
+    print("I am", id_set[id_self])
 
     try:
         with client.pubsub.subscribe(channel) as sub:
             if listening:
                 for msg in sub:
                     data = base64.b64decode(msg['data']).decode('utf-8')
-                    sender = msg['from']
-                    if sender not in id_set:
-                        print('#### |', sender, 'entered the room')
-                        id_set.add(sender)
-                    if sender == id_self:
-                        print('SENT |', data)
+                    id_sender = msg['from']
+                    if id_sender not in id_set:
+                        id_set[id_sender] = avatars[avatar_idx]
+                        avatar_idx += 1
+                        print('# | ', id_set[id_sender], 'entered the room')
+                    if id_sender == id_self:
+                        print(id_set[id_sender], '|', data)
                     else:
-                        print('RECV |', data)
+                        print(id_set[id_sender], '|', data)
             else:
                 return
     except Exception as e:
         print(e)
-        global sending
         sending = False
         return
 
@@ -54,7 +61,6 @@ def send(client, channel):
 if __name__ == '__main__':
     client = ipfshttpclient.connect()
     channel = 'Chat Room 1'
-
     print('---' + channel + '---')
 
     t1 = Thread(target=listen, args=[client, channel])
@@ -65,4 +71,3 @@ if __name__ == '__main__':
 
     t2.join()
     print("---End---")
-    exit()
